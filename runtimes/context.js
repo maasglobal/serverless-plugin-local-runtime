f6d778f6f8c6f1402b8fb9445fe3a0ff41e57a87
Hieu Nguyen <blackevil245@gmail.com>
2017-02-14 17:56:11 +0200

    Require callback as done

--- runtimes/context.js
+++ runtimes/context.js
@@ -1,26 +1,5 @@
 'use strict';
 
 module.exports = {
-  done: (err, result) => {
-
-    // Show error
-    if (err) {
-      console.log('Failed - This Error Was Returned:');
-      console.log(err.message);
-      console.log(err.stack);
-
-      console.log(JSON.stringify({
-        status: 'error',
-        response: err.message,
-        error: err,
-      }, null, 2));
-    }
-
-    // Show success response
-    console.log('Success! - This Response Was Returned:');
-    console.log(JSON.stringify({
-      status: 'success',
-      response: result,
-    }, null, 2));
-  },
+  done: require('./callback'),
 };
