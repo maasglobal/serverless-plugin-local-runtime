9f3331d18079f2dc37cfcbe7e52dfc4d551c67f4
laurisvan <lauri.svan@iki.fi>
2017-04-24 17:31:29 +0300

    Support babelification; fix async return & constructor bugs

--- runtimes/LocalNodeRuntime.js
+++ runtimes/LocalNodeRuntime.js
@@ -14,34 +14,43 @@ module.exports = function (S) {
 
   class LocalNodeRuntime extends S.classes.RuntimeNode43 {
 
+    constructor() {
+      super();
+      const project = S.getProject();
+      const config = project.custom.runtime[LocalNodeRuntime.getName()] || {};
+
+      // Add the defaults
+      this.flags = config.flags || [];
+      this.babelify = typeof config.babelify === 'boolean' ? config.babelify : false;
+      this.name = config.name || 'nodejs6.10';
+    }
+
     static getName() {
       return 'local-node';
     }
 
     getName() {
-      return 'nodejs6.10';
+      return this.name;
     }
 
     run(func, stage, region, event) {
-
       const project = S.getProject();
-      let runtimeFlags = [];
-      if (Array.isArray(project.custom.runtime[LocalNodeRuntime.getName()].flags)) {
-        runtimeFlags = project.custom.runtime[LocalNodeRuntime.getName()].flags;
-      }
+      const flags = this.flags;
+      const babelify = this.babelify;
 
       return this.getEnvVars(func, stage, region)
           // Add ENV vars (from no stage/region) to environment
           .then(env => {
-            const handlerArr  = func.handler.split('/').pop().split('.');
+            const handlerArr = func.handler.split('/').pop().split('.');
             const functionFile = func.getRootPath(handlerArr[0] + (func.handlerExt || '.js'));
             const functionHandler = handlerArr[1];
             const functionRelativePath = path.relative(process.cwd(), functionFile);
 
             // callback file must be required by evaluation, therefore pass only require string to evalQuery
             // const callback = 'require("serverless-plugin-local-runtime/runtimes/callback")';
-
+            const babelOrNot = babelify ? 'require(\'babel-register\');' : '';
             const evaluateQuery = `
+              ${babelOrNot}
               require("./${functionRelativePath}")
                 .${functionHandler}
                   (
@@ -60,11 +69,8 @@ module.exports = function (S) {
                 evaluateQuery,
               ];
 
-              // Use project defined flags as args
-              runtimeFlags.forEach(flag => (childArgs.push(flag)));
-
               // Child process to eval the code
-              const child = spawn('node', childArgs, { env: _.merge(env, process.env) });
+              const child = spawn('node', childArgs.concat(flags), { env: _.merge(env, process.env) });
               let responseReceived = false;
               child.stdout.on('data', data => {
                 if (data.includes('Success! - This Response Was Returned:')) { responseReceived = true; }
@@ -80,8 +86,10 @@ module.exports = function (S) {
                 // Convert from seconds and nanoseconds to milliseconds
                 const duration = endTime[0] * 1000 + endTime[1] / 1000000;
                 SCli.log('Duration: ' + duration.toFixed(2) + ' ms');
+
+                // Return from execution
+                resolve();
               });
-              resolve();
             });
           })
           .catch(err => {
